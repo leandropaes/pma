cd87723ce4356b995752c8db149a17e6204ac28c
Leandro Lima <ext.leandro.lima@dafiti.com.br>
2020-02-04 13:41:11 -0300

    - Exibindo lista de projetos e atividades na hora de realizar o apontamento de horas

--- scripts/app.bash
+++ scripts/app.bash
@@ -23,9 +23,9 @@ Por favor, informe os campos abaixo no arquivo:$CL_YELLOW scripts/configs.bash$C
 app_project()
 {
     case ${2} in
-        "list") app_project_list ;;
-        "items") app_project_items ${@} ;;
-        "refresh") > $PROJECTS_FILE; app_project_list ;;
+        "list") logo; app_project_list ;;
+        "items") logo; app_project_items ${3} ;;
+        "refresh") > $PROJECTS_FILE; logo; app_project_list ;;
         *) menu_project ;;
     esac
 }
@@ -37,21 +37,17 @@ app_project_list()
         app_project_list_refresh
     fi
 
-    logo 
-
     out "
 =====================================================================
 $CL_YELLOW COD$CL_DEFAULT\t | \t$CL_YELLOW PROJECT$CL_DEFAULT
 ====================================================================="
     cat $PROJECTS_FILE
-
-    echo
 }
 
 app_project_list_refresh()
 {
     login
-    
+
     # list projects
     curl -s -b $COOKIE_FILE GET {$URL}/registros \
         | sed -n '/<select id="registro_projeto_id"/,/<\/select/p' \
@@ -62,8 +58,9 @@ app_project_list_refresh()
 
 app_project_items()
 {
-    # login
-    PROJECT_COD=${3}
+    login
+    
+    PROJECT_COD=${1}
 
     if [ -z "$PROJECT_COD" ] ; then
         logo
@@ -83,11 +80,10 @@ app_project_items()
         | sed -e 's/<option value=//g' -e 's/<\/option>//g' -e 's/">/\t | \t /g' -e 's/"/ /g' -e 's/<\/select>//g' \
         > $CURL_RESPONSE_FILE
 
-    logo 
-
     if [ ! -s "$CURL_RESPONSE_FILE" ] ; then
         echo -e "$CL_YELLOW\nNão há atividades atribuídas para você neste projeto.\nNão será possível efetuar apontamentos. Por favor, contate o gerente do projeto.$CL_DEFAULT"
         echo
+        exit 1
     else
         out "
 =====================================================================
@@ -112,21 +108,34 @@ app_new()
         exit 1
     fi
 
+    # Description input
     echo -e $CL_YELLOW"Informe a descrição [ENTER para utilizar a padrão]:\n$CL_DEFAULT(Descrição padrão:$CL_GREN $DESCRIPTION_DEFAULT"$CL_DEFAULT")"
     read DESCRIPTION
+    echo
 
     if [ -z "$DESCRIPTION" ] ; then
         DESCRIPTION=$DESCRIPTION_DEFAULT
     fi
 
+    # Project input
     echo -e $CL_YELLOW"Informe o código do projeto [ENTER para utilizar a padrão]:\n$CL_DEFAULT(Projeto padrão:$CL_GREN $PROJECT_DEFAULT"$CL_DEFAULT")"
+    app_project_list
+    echo
     read PROJECT
 
     if [ -z "$PROJECT" ] ; then
         PROJECT=$PROJECT_DEFAULT
     fi
 
-    echo -e $CL_YELLOW"Informe o código da atividade do projeto [ENTER para utilizar a padrão]:\n$CL_DEFAULT(Atividade padrão:$CL_GREN $ACTIVITY_DEFAULT"$CL_DEFAULT")"
+    # Activity input
+    if [ $PROJECT != $PROJECT_DEFAULT ]; then
+        echo -e $CL_YELLOW"Informe o código da atividade do projeto:$CL_DEFAULT"
+    else
+        echo -e $CL_YELLOW"Informe o código da atividade do projeto [ENTER para utilizar a padrão]:\n$CL_DEFAULT(Atividade padrão:$CL_GREN $ACTIVITY_DEFAULT"$CL_DEFAULT")"
+    fi
+
+    app_project_items $PROJECT
+    echo
     read ACTIVITY
 
     if [ -z "$ACTIVITY" ] ; then
@@ -150,6 +159,7 @@ app_new()
     MSG_ERROR=$(cat $CURL_RESPONSE_FILE | sed -n 's:.*id="msgNotice">\(.*\)</p>.*:\1:p')
 
     if [ -z "$MSG_ERROR" ] ; then
+        echo $MSG_ERROR
         echo -e $CL_GREN"Apontamento realizado com sucesso!\n"$CL_DEFAULT
     else
         echo -e $CL_YELLOW"$MSG_ERROR\n"$CL_DEFAULT
